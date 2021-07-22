d12b396b86ed7f4f8fa46d6d4a32d57edaf1d3bd
Moddy Pikiso <moddypikiso17@gmail.com>
2021-07-23 00:21:16 +0200

    first commit

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,1 +1,14 @@
 var functionToTest = require('../greet');
+
+
+describe('The greet function', function(){
+
+    it('should greet Andrew correctly', function(){
+        assert.equal('Hello, Andrew', 'Hello, Andrew');
+    });
+
+    it('should greet Karen correctly', function(){
+        // this test will fail - can you fix it?
+        assert.equal('Hello, Karen', 'Hello, Karen');
+    });
+});
